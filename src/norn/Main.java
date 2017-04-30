e2f97d5335428682ecfe2f9f34efe6b5fcb427aa
rcm via Didit <rcm@mit.edu>
2017-04-30 19:03:44 -0400

    Starting code for projects/norn2

---
+++ src/norn/Main.java
@@ -0,0 +1,15 @@
+package norn;
+
+/**
+ * This class starts the Norn system.
+ */
+public class Main {
+    
+    
+    /**
+     * TODO
+     */
+    public static void main(String[] args) {
+    }
+ 
+}
