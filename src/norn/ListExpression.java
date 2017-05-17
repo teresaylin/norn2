a81d25b42691608b7eb462cf77dc6a06e702de4b
tlin15 <tlin15@mit.edu>
2017-05-17 01:35:30 -0400

    Updated specs for getDependents

--- src/norn/ListExpression.java
+++ src/norn/ListExpression.java
@@ -48,12 +48,15 @@ public interface ListExpression {
      * For the current ListExpression, get the set of ListExpressions that it depends on.
      * If the current ListExpression does not depend on any ListExpressions, then an empty set
      * is returned.
-     * @param environment the environment to use to get children
      * @return the set of ListExpressions that make up a ListExpression
      */
     public Set<ListExpression> getChildren();
     
-    
+    /**
+     * For a listname, gets the ListExpression(s) associated with that listname.
+     * @param environment the environment containing all stored listnames
+     * @return the set of ListExpressions that make up a listname
+     */
     public Set<ListExpression> getDependents(Environment environment);
     
     /**
